8449c8861bab2ba8905b662a7948c34d66e8152a
rubataga <isaactherubataga@gmail.com>
2018-11-19 07:19:00 -0600

    make it better

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./settings.json');
+const token = require('./settings.json').token;
+var prefix = '~';
+var prefix2 = 'kb.';
 
 client.on('ready',() => {
-  console.log('I\'m Online/nI\'m Online');
+  console.log('This is it chief.');
 });
 
-client.on('message',message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+
+client.on('message', message => {
+  if(!message.content.startsWith(prefix)) return;
+  console.log("prefix detected");
+  if (message.author.bot) return;
+
+  //commands
+  if (message.content === prefix + 'ping') {
+    message.channel.send('pong');
   }
+
+  //the commander is double gay
+  elseif (message.content === prefix + 'nohomo') {
+    message.channel.send('^ is extra double gay');
+  }
+
+  //respond to hey
+  elseif (message.content === prefix + 'hey') {
+    message.channel.send('hi');
+  }
+
+  //repeat back the command
+  elseif (message.content === prefix + 'repeat') {
+    message.channel.send(message.content);
+  }
+
+  //uwu reponse
+  elseif (message.content === prefix + 'uwu'){
+    message.channel.send('st*u weeaboo scum');
+    message.channel.send('jk chieftains that was a prank of course uwu uwu uuuwd');
+  }
+
+  //send an embed of Kenji
+  elseif(message.content === prefix + 'irlkenji'){
+    const embed = new Discord.RichEmbed()
+      .setTitle('KenjiBot IRL')
+      .setColor(0xC90808)
+      .setDescription("Wow! It's Kenji!")
+      .setImage('https://i.imgur.com/8RkPa1X.jpg')
+      .setFooter('In awe of this absolute unit.')
+      .setThumbnail('https://i.imgur.com/8RkPa1X.jpg');
+    message.channel.send(embed);
+  }
+
 });
 
-client.login(settings.token);
+client.login(token);
