9dcec567caa05a86381262ef6c423caa3e392b75
Unknown <icyang03@gmail.com>
2018-11-19 12:24:11 -0600

    no mercy lyrics/sicko mode
    added a response for the no mercy lyrics as well as a sicko mode command

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ client.on('message', message => {
   }
   if (!prefix)
     return
-  
+
   if (message.author.bot) return;
 
   //commands
@@ -44,8 +44,8 @@ client.on('message', message => {
     message.channel.send(embed);
   }
 
-  else if (message.content === prefix + 'bruh2') {
-    message.channel.send('bruh!')
+  else if (message.content === 'bruh') {
+    message.channel.send('this is a "bruh" moment!')
     message.channel.send('https://www.youtube.com/watch?v=2ZIpFytCSVc')
     //https://www.youtube.com/watch?v=2ZIpFytCSVc
   }
@@ -71,14 +71,41 @@ client.on('message', message => {
 
   //respond to hey
   else if (message.content === prefix + 'hey') {
-    message.channel.send('hi');
+    message.channel.send('hi')
   }
 
+  else if (message.content === 'sicko mode'){
+    message.channel.send('SICKO MODE!')
+    message.channel.send('https://www.youtube.com/watch?v=6ONRf7h3Mdk')
+  }
   //repeat back the command ~repeat a
   else if (message.content.startsWith(prefix + 'repeat')) {
     message.channel.send(message.content.subStr(8));
   }
 
+  else if (message.content === "maybe i'll be tracer"){
+    message.channel.send("i'm already tracer")
+  }
+
+  else if (message.content === "what about widowmaker"){
+    message.channel.send("i'm already widowmaker")
+  }
+
+  else if (message.content === "i'll be bastion"){
+    message.channel.send("NERF BASTION")
+  }
+
+  else if (message.content === "you're right, so winston"){
+    message.channel.send("i wanna be winston")
+  }
+
+  else if (message.content === "i guess i'll be genji"){
+    message.channel.send("i'm already genji")
+  }
+
+  else if (message.content === "then i'll be mccree"){
+    message.channel.send("i already chose mccree")
+  }
   //uwu reponse
   else if (message.content === prefix + 'uwu'){
     message.channel.send('st*u weeaboo scum');
