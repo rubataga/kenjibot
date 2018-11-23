b5541e17f02a7224970f1ec1836261074ed54b83
rubataga <icyang03@gmail.com>
2018-11-22 23:15:24 -0600

    killbot doesnt work online
    rejects the command if run through heroku

--- commands/killbot.js
+++ commands/killbot.js
@@ -5,8 +5,14 @@ module.exports = {
       console.log(1)
       if (message.member.roles.has('515280398037811204')) {
         console.log(2)
-        await message.channel.send("bruh this can't be happening")
-        process.exit()
+        if (!(process.env.NODE && ~process.env.NODE.indexOf("heroku"))){
+          await message.channel.send("bruh this can't be happening")
+          process.exit()
+        } else {
+          message.channel.send("you can't run this command while the bot is hosted online!")
+        }
+      } else {
+        message.channel.send("you aren't a botmaster!")
       }
       console.log(3)
     },
