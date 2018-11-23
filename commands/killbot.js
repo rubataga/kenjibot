183b49f7d956e8eb979a3d5176fa6676b5d010e5
Slick <jacobfarrell18@gmail.com>
2018-11-22 22:55:34 -0600

    should work now

--- commands/killbot.js
+++ commands/killbot.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'killbot',
     description: 'kills KenjiBot :(',
-    execute(message, args) {
+    async execute(message, args) {
       console.log(1)
       if (message.member.roles.has('515280398037811204')) {
         console.log(2)
