eda51fdc98027ff3ea64af0beaf39c361c85dfc9
Slick <jacobfarrell18@gmail.com>
2018-11-22 22:49:44 -0600

    killbot should now print message

--- commands/killbot.js
+++ commands/killbot.js
@@ -5,7 +5,7 @@ module.exports = {
       console.log(1)
       if (message.member.roles.has('515280398037811204')) {
         console.log(2)
-        message.channel.send("bruh this can't be happening")
+        await message.channel.send("bruh this can't be happening")
         process.exit()
       }
       console.log(3)
