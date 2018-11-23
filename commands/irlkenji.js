4e58222fd642afd168940e3bfc182e9fff78ccc0
rubataga <icyang03@gmail.com>
2018-11-23 00:07:41 -0600

    fixed irl kenji
    irl kenji now actually uploads the kenji image file

--- commands/irlkenji.js
+++ commands/irlkenji.js
@@ -1,14 +1,16 @@
+const Discord = require('discord.js');
 module.exports = {
     name: 'irlkenji',
     description: 'shows Kenji irl!',
     execute(message, args) {
       const embed = new Discord.RichEmbed()
+        .attachFiles([new Discord.Attachment('./media/kenjiride.PNG')])
         .setTitle('KenjiBot IRL')
         .setColor(0xC90808)
         .setDescription("Wow! It's Kenji!")
-        .setImage('./media/kenjiride.PNG)
+        .setImage('attachment://kenjiride.PNG')
         .setFooter('In awe of this absolute unit.')
-        .setThumbnail('./media/kenjiride.PNG');
+        .setThumbnail('attachment://kenjiride.PNG');
     message.channel.send(embed);
     },
 };
